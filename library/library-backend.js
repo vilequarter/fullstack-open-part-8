a40ed87c53eaae62bd3d5b222e5763954a3d751f
vilequarter <daparady@gmail.com>
2024-07-25 13:45:31 +0300

    exercise 8.5

--- library/library-backend.js
+++ library/library-backend.js
@@ -106,7 +106,7 @@ const typeDefs = `
   }
 
   type Query {
-    allBooks(author: String!): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
   }
 
   type Book {
@@ -127,11 +127,17 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if(!args.author){
+      if(!(args.author || args.genre)){
         return books
       }
-      const ownBooks = books.filter(book => book.author === args.author)
-      return ownBooks
+      let filteredBooks = books
+      if(args.author){
+        filteredBooks = filteredBooks.filter(book => book.author === args.author)
+      }
+      if(args.genre){
+        filteredBooks = filteredBooks.filter(book => book.genres.includes(args.genre))
+      }
+      return filteredBooks
     },
     allAuthors: () => {
       return authors
