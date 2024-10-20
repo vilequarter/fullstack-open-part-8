5b18ffa23d371ef06fa6a73d6679fa7bb9ac2576
vilequarter <daparady@gmail.com>
2024-10-20 09:05:37 +0300

    exercise 8.9

--- library/library-frontend/src/App.jsx
+++ library/library-frontend/src/App.jsx
@@ -2,17 +2,9 @@ import { useState } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
-import { ALL_PERSONS } from "./components/queries"
-import { useQuery } from "@apollo/client"
 
 const App = () => {
   const [page, setPage] = useState("authors");
-
-  const result = useQuery(ALL_PERSONS)
-
-  if(result.loading) {
-    return <div>loading...</div>
-  }
 
   return (
     <div>
@@ -22,7 +14,7 @@ const App = () => {
         <button onClick={() => setPage("add")}>add book</button>
       </div>
 
-      <Authors show={page === "authors"} authors={result.data.allAuthors} />
+      <Authors show={page === "authors"} />
 
       <Books show={page === "books"} />
 
--- library/library-frontend/src/components/queries.js
+++ library/library-frontend/src/components/queries.js
@@ -1,11 +1,21 @@
 import { gql } from '@apollo/client'
 
-export const ALL_PERSONS = gql`
+export const ALL_AUTHORS = gql`
   query {
     allAuthors {
       name
       born
       bookCount
+    }
+  }
+`
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
     }
   }
 `
